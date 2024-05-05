b72b6fb2bd1fd9c91f6493cdb42c11f885527f6d
VitorBatista2324 <147276976+VitorBatista2324@users.noreply.github.com>
2024-05-05 14:25:01 -0300

    Update Main.java

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,27 +1,27 @@
-package Main;// Importações necessárias
+package Main;
 import java.util.Scanner;
 import Enums.TipoMotoEnum;
 import Enums.TipoCarroEnum;
 import Enums.TipoEstradaEnum;
 import Model.*;
 
-// Classe principal do programa
+
 public class Main {
     public static void main(String[] args) {
-        // Inicialização do scanner para entrada de dados do usuário
+
         Scanner scanner = new Scanner(System.in);
 
-        // Mensagem de boas-vindas ao sistema de corrida
+
         System.out.println("Bem-vindo ao sistema de corrida!");
 
-        // Solicitação ao usuário para informar o tipo de terreno da corrida
+        // para informar o tipo de terreno
         System.out.println("Informe o tipo de terreno da corrida:");
         System.out.println("1. Asfalto");
         System.out.println("2. Terra");
         System.out.println("3. Grama");
         int opcaoTerreno = scanner.nextInt();
 
-        // Variável para armazenar o tipo de terreno da corrida
+        // para armazenar o tipo de terreno da corrida
         TipoEstradaEnum tipoTerreno;
         switch (opcaoTerreno) {
             case 1:
@@ -34,41 +34,41 @@ public class Main {
                 tipoTerreno = TipoEstradaEnum.BARRO;
                 break;
             default:
-                // Caso o usuário forneça uma opção inválida, o programa utiliza o Asfalto como padrão
+
                 System.out.println("Opção inválida. Usando Asfalto como padrão.");
                 tipoTerreno = TipoEstradaEnum.ASFALTO;
                 break;
         }
 
-        // Criação dos veículos para a corrida
+        // duas instâncias chamando um método criarVeiculo para criar dois objetos de veículo, veiculo1 e veiculo2.
         Veiculo veiculo1 = criarVeiculo(scanner, "primeiro");
         Veiculo veiculo2 = criarVeiculo(scanner, "segundo");
 
-        // Criação da corrida com os veículos e o tipo de terreno informado
+        // criando a corrida com os veículos e o tipo de terreno
         Corrida corrida = new Corrida(veiculo1, veiculo2, tipoTerreno);
 
-        // Ações antes do início da corrida
+        // chamando Contratos
         corrida.acao1();
         corrida.acao2();
 
-        // Mensagem indicando que a corrida está prestes a começar
+        // iniciando a corrida
         System.out.println("Preparando para começar a corrida...");
         corrida.iniciar();
 
-        // Determinação do vencedor da corrida
+        // determinar o ganhador
         System.out.println("Determinando o vencedor...");
         String vencedor = corrida.Logica();
         System.out.println("O vencedor da corrida é: " + vencedor);
 
-        // Finalização da corrida
+        // final
         System.out.println("Finalizando a corrida...");
         corrida.finalizar();
 
-        // Fechamento do scanner para liberar recursos
+
         scanner.close();
     }
 
-    // Método para criar um veículo com base nas informações fornecidas pelo usuário
+    // metodo para criar um veiculo
     private static Veiculo criarVeiculo(Scanner scanner, String ordem) {
         System.out.println("Informe os detalhes do " + ordem + " veículo:");
         System.out.println("Marca do veículo:");
@@ -85,53 +85,51 @@ public class Main {
         scanner.nextLine();
 
         if (tipoVeiculo == 1) {
-            return criarMoto(scanner, marca, modelo, potencia);
+            return criarMoto(scanner, marca, modelo, potencia); // chama o metodo para criar uma moto
         } else if (tipoVeiculo == 2) {
-            return criarCarro(scanner, marca, modelo, potencia);
+            return criarCarro(scanner, marca, modelo, potencia); // para criar um carro
         } else {
-            // Se o tipo de veículo informado for inválido, cria uma moto como padrão
             System.out.println("Tipo de veículo inválido. Criando moto como padrão.");
             return criarMoto(scanner, marca, modelo, potencia);
         }
     }
 
-    // Método para criar uma moto com base nas informações fornecidas pelo usuário
+    // metodo para criar uma moto
     private static Veiculo criarMoto(Scanner scanner, String marca, String modelo, int potencia) {
-        System.out.println("Tipo de moto (1 para Moto1, 2 para Moto2):");
+        System.out.println("Tipo de moto (1 para ESPORTIVA, 2 para BIG_TRAIL):");
         int tipoMoto = scanner.nextInt();
         TipoMotoEnum tipoMotoEnum;
         switch (tipoMoto) {
             case 1:
                 tipoMotoEnum = TipoMotoEnum.ESPORTIVA;
-                return new Moto1(marca, modelo, tipoMotoEnum, potencia);
+                return new Moto1(marca, modelo, tipoMotoEnum, potencia); // define o tipo de moto como esportiva
             case 2:
                 tipoMotoEnum = TipoMotoEnum.BIG_TRAIL;
-                return new Moto2(marca, modelo, tipoMotoEnum, potencia);
+                return new Moto2(marca, modelo, tipoMotoEnum, potencia); // define como big trail
             default:
-                // Se o tipo de moto informado for inválido, cria uma Moto1 como padrão
                 System.out.println("Tipo de moto inválido. Escolhendo Moto1 como padrão.");
                 tipoMotoEnum = TipoMotoEnum.ESPORTIVA;
                 return new Moto1(marca, modelo, tipoMotoEnum, potencia);
         }
     }
 
-    // Método para criar um carro com base nas informações fornecidas pelo usuário
+    // metodo para criar um carro
     private static Veiculo criarCarro(Scanner scanner, String marca, String modelo, int potencia) {
-        System.out.println("Tipo de carro (1 para Carro1, 2 para Carro2):");
+        System.out.println("Tipo de carro (1 para ESPORTIVO, 2 para OFF_ROAD):");
         int tipoCarro = scanner.nextInt();
         TipoCarroEnum tipoCarroEnum;
         switch (tipoCarro) {
             case 1:
                 tipoCarroEnum = TipoCarroEnum.ESPORTIVO;
-                return new Carro1(marca, modelo, tipoCarroEnum, potencia);
+                return new Carro1(marca, modelo, tipoCarroEnum, potencia); // cria e retorna um carro
             case 2:
                 tipoCarroEnum = TipoCarroEnum.OFF_ROAD;
-                return new Carro2(marca, modelo, tipoCarroEnum, potencia);
+                return new Carro2(marca, modelo, tipoCarroEnum, potencia); // cria e retorna outro carro
             default:
-                // Se o tipo de carro informado for inválido, cria um Carro1 como padrão
                 System.out.println("Tipo de carro inválido. Escolhendo Carro1 como padrão.");
                 tipoCarroEnum = TipoCarroEnum.ESPORTIVO;
                 return new Carro1(marca, modelo, tipoCarroEnum, potencia);
         }
     }
 }
+
